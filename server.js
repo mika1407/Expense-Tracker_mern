67aee88a5af4f259cd9a6e12cda2dde7010479da
mika tiihonen <koneautomaatio@hotmail.com>
2024-03-13 19:43:27 +0200

    GET and POST done

--- server.js
+++ server.js
@@ -3,18 +3,18 @@ const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
 const morgan = require('morgan');
-// const connectDB = require('./config/db');
+const connectDB = require('./config/db');
 
 dotenv.config({ path: './config/config.env' });
 
-// connectDB();
+connectDB();
 
 const transactions = require('./routes/transactions');
 
 const app = express();
 
 
-// app.use(express.json());
+app.use(express.json());
 
 // if(process.env.NODE_ENV === 'development') {
 //   app.use(morgan('dev'));
